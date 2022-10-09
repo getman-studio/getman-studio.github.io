dd9b65eec2cdc2cafd73e6972cd1357c524f33d0
David Leibovych <david.leibovych@anonybit.io>
2022-10-09 19:12:05 +0300

    fixes quotes

--- components/sculpture.js
+++ components/sculpture.js
@@ -6,7 +6,7 @@ export default function Sculpture() {
             <p className={"text-2xl text-white mb-12"}>Скульптура</p>
             <LargeItem image={"/portfolio/sculpture_1.png"} description={
                 <div className={"p-4"}>
-                    <p className={"text-black"}>Скульптура з дерева "Родина"</p>
+                    <p className={"text-black"}>Скульптура з дерева &quot;Родина&quot;</p>
                     <br />
                     <a className={"text-blue"} href={"https://www.facebook.com/getmanstudio/photos/a.962635990474690/2984543931617209/?type=3&theater"}>https://www.facebook.com/getmanstudio/photos/a.962635990474690/2984543931617209/?type=3&theater</a>
                 </div>
