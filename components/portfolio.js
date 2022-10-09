06a9323eec06e050d9cdc688336967ccd13ecf32
David Leibovych <david.leibovych@anonybit.io>
2022-10-09 21:28:37 +0300

    fixes cursor appearance

--- components/portfolio.js
+++ components/portfolio.js
@@ -7,34 +7,34 @@ export default function Portfolio() {
 
     return (
         <div className={"flex flex-col md:flex-row gap-1 md:gap-8 pl-4 pr-4 md: overflow-x-scroll"}>
-            <a onClick={() => router.push('?page=sculpture', undefined, { shallow: true})}>
+            <a onClick={() => router.push('?page=sculpture', undefined, {shallow: true})} className={"cursor-pointer"}>
                 <PortfolioItem
-                    foregroundImage={"/portfolio/Sculpture Top.png"}
-                    backgroundImage={"/portfolio/Sculpture Bottom.png"}
+                    foregroundImage={"/portfolio/sculpture_top.png"}
+                    backgroundImage={"/portfolio/sculpture_bottom.png"}
                     title={"Скульптура"}
                 />
             </a>
-            <a href={"/mural"}>
+            <a onClick={() => router.push('?page=mural', undefined, {shallow: true})} className={"cursor-pointer"}>
                 <PortfolioItem
                     title={"Розпис"}
                 />
             </a>
-            <a href={"/painting"}>
+            <a onClick={() => router.push('?page=painting', undefined, {shallow: true})} className={"cursor-pointer"}>
                 <PortfolioItem
                     title={"Живопис"}
                 />
             </a>
-            <a href={"/decoration"}>
+            <a onClick={() => router.push('?page=decoration', undefined, {shallow: true})} className={"cursor-pointer"}>
                 <PortfolioItem
                     title={"Декорації"}
                 />
             </a>
-            <a href={"/graphic"}>
+            <a onClick={() => router.push('?page=graphic', undefined, {shallow: true})} className={"cursor-pointer"}>
                 <PortfolioItem
                     title={"Графіка"}
                 />
             </a>
-            <a href={"/art"}>
+            <a onClick={() => router.push('?page=art', undefined, {shallow: true})} className={"cursor-pointer"}>
                 <PortfolioItem
                     title={"Арт"}
                 />
