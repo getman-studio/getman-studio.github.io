23e6404549bc2e2a6572e0d71a39215cfb4cd253
David Leibovych <david.leibovych@anonybit.io>
2022-10-09 22:21:42 +0300

    sculptures

--- components/sculpture.js
+++ components/sculpture.js
@@ -11,6 +11,96 @@ export default function Sculpture() {
                     <a className={"text-blue"} href={"https://www.facebook.com/getmanstudio/photos/a.962635990474690/2984543931617209/?type=3&theater"}>https://www.facebook.com/getmanstudio/photos/a.962635990474690/2984543931617209/?type=3&theater</a>
                 </div>
             }/>
+            <br />
+            <LargeItem image={"/portfolio/sculpture_2.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзові шахи для &quot;HORSE CLUB&quot; бронза, дуб. Замовник: <a className={"text-blue"} href={"https://mainstream.ua/ua/"}>https://mainstream.ua/ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_3.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для конкурсу «PRESSЗВАНИЕ» &quot;FAST FOOD&quot; ; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_4.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Нагорода &quot;LOFT&quot; для конкурсу «PRESSЗВАНИЕ» ; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_5.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для конкурсу «PRESSЗВАНИЕ» «9 вал; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_6.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для конкурсу «PRESSЗВАНИЕ» «8 сезон»; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_7.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова скульптура для METINVEST «5 years»; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://mainstream.ua/en/"}>http://mainstream.ua/en/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_8.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова скульптура для «ДТЕК 6 років успіху»; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://mainstream.ua/en/"}>http://mainstream.ua/en/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_9.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для конкурсу «PRESSЗВАНИЕ», замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_10.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Сталева нагорода для конкурсу «PRESSЗВАНИЕ» Дніпро, замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_11.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Сталева скульптура для «Дельта-Лоцман» «Маяк», замовник: <a className={"text-blue"} href={"http://mainstream.ua/en/"}>http://mainstream.ua/en/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_12.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для METINVEST «Gold supply», замовник: <a className={"text-blue"} href={"http://mainstream.ua/en/"}>http://mainstream.ua/en/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_13.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для конкурсу «PRESSЗВАНИЕ»; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_14.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода для конкурсу «PRESSЗВАНИЕ» «ГМО»; скульптор: Гетьман Валентин; замовник: <a className={"text-blue"} href={"http://www.presszvanie.ua/"}>http://www.presszvanie.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_15.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода, логотип ВО «Альфа-СБТ», замовник: <a className={"text-blue"} href={"http://www.otich.com.ua/"}>http://www.otich.com.ua/</a></p>
+                </div>
+            }/>
+            <br/>
+            <LargeItem image={"/portfolio/sculpture_16.png"} description={
+                <div className={"p-4"}>
+                    <p className={"text-black"}>Бронзова нагорода, логотип ВО «Альфа-СБТ», замовник: <a className={"text-blue"} href={"http://www.otich.com.ua/"}>http://www.otich.com.ua/</a></p>
+                </div>
+            }/>
             <br/>
         </div>
     )
